48ff4abcf444a4b8ff205a23c850b093259675fa
brianqian <qian.brian@gmail.com>
2022-09-07 22:31:28 -0400

    Finish with lib cli

--- library-cli/index.go
+++ library-cli/index.go
@@ -1,3 +1,5 @@
+// Trying to build without receiver functions and get practice in with syntax
+
 package main
 
 import (
@@ -15,11 +17,9 @@ func main() {
 	status := "question"
 
 	for status != "exit" {
-		status := getUserSelection()
-		s, l := handleSelection(status, library)
+		s, l := handleSelection(getUserSelection(), library)
 		library = l
 		status = s
-		fmt.Println("after handleSelection", library)
 	}
 
 	return
@@ -27,26 +27,28 @@ func main() {
 }
 
 func handleSelection(status string, lib []Book) (s string, l []Book) {
-	fmt.Println("handling status", status)
 	switch status {
 	case "add":
 		book := getBookInfo()
 		lib = append(lib, book)
-		return "question", lib
-
+	case "list":
+		for _, v := range lib {
+			fmt.Println(v)
+		}
 	case "remove":
-
+		fmt.Println("functionality TBD")
 	case "edit":
-
+		fmt.Println("functionality TBD")
 	case "seed":
-
+		book1 := Book{"Sample Book 1", "John Smith", true}
+		book2 := Book{"Money", "Adam Smith", true}
+		book3 := Book{"Waterfalls", "Calvin Harris", false}
+		lib = append(lib, book1, book2, book3)
 	case "exit":
-
-		return "exit", lib
-	default:
 		return "exit", lib
 	}
-	return
+	return "question", lib
+
 }
 
 func getUserSelection() string {
@@ -56,7 +58,8 @@ func getUserSelection() string {
 		"2": "remove",
 		"3": "edit",
 		"4": "seed",
-		"5": "exit",
+		"5": "list",
+		"6": "exit",
 	}
 
 	fmt.Println("What would you like to do?")
@@ -64,7 +67,8 @@ func getUserSelection() string {
 	fmt.Println("2. Remove a book")
 	fmt.Println("3. Edit a book")
 	fmt.Println("4. Seed library")
-	fmt.Println("5. Exit Library")
+	fmt.Println("5. List Library")
+	fmt.Println("6. Exit Library")
 	fmt.Scan(&choice)
 
 	return choiceMap[choice]
